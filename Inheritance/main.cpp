b4226d9169039e9b723bfd1c3e4820f9048100b8
Piyush-shoundik <ritetopiyush@gmail.com>
2026-03-13 00:22:06 +0530

    single inheritance

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,1 +1,25 @@
 // Starting with inhertance
+// single inheritance 
+
+#include <iostream>
+using namespace std;
+
+class a{
+
+    protected:
+    int x = 10;
+};
+
+class b:private a{
+    public:
+    int y = 10;
+
+    void display(){
+        cout << "total is " << x + y ;
+    }
+};
+
+int main(){
+    b aa;
+    aa.display();
+}
