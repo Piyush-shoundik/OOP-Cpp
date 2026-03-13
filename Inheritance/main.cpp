119793822a249544f0d00e77a92cad9b33cfc9d1
Piyush-shoundik <ritetopiyush@gmail.com>
2026-03-14 00:56:25 +0530

    constructor handling in inheritance

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,5 +1,5 @@
 // Starting with inhertance
-// method overriding
+// constructor handling in inheritance
 
 #include <iostream>
 #include <string>
@@ -8,45 +8,30 @@ using namespace std;
 
 class a{
     public:
-    // int x;
-    void input(){
-        cout << "This is parent class " << endl;
+    a(int x){
+        cout << "This is parent class  " << endl;
+        cout << "value of x is "<< x << endl;
     }
     
 };
 
 class b:public a{
     public:
-    // int y;
-
-    // b(){
-    //     cout << "Give input for second";
-    //     cin >> y;
-    // }
-
-    void input(){
-        cout << "This is child class " << endl;
+    b(int x): a(x) {
+        cout << "This is child class 1 " << endl;
     }
 };
 
-// class c:public a{
-//     public:
-//     int z;
-    
-    
-//     // void plus(){
-//     //     z = x + y;
-//     // }
-    
-//     void display(){
-//         cout << "value of x in class c " << x << endl;
-//     }
-// };
+class c:public b{
+    public:
+    c(int x,int y): b(x) {
+        cout << "This is child class 2 " << endl;
+        cout << "value of y is "<< y << endl;
+    }
+};
 
 int main(){
-    b bb;
-    bb.input();
-    bb.a::input(); //this is to call function of parent class
-    // cc.plus();
-    // cc.display();
+    c bb(10,30);
+    // bb.input();
+    // bb.a::input(); //this is to call function of parent class
 }
