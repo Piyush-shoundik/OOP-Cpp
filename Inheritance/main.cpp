a6bb10613f733698449abe0f3ced2e6a0e7c5362
Piyush-shoundik <ritetopiyush@gmail.com>
2026-03-15 02:35:55 +0530

    using of virtual function

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,5 +1,5 @@
 // Starting with inhertance
-// constructor handling in inheritance
+// using of virtual function
 
 #include <iostream>
 #include <string>
@@ -8,31 +8,33 @@ using namespace std;
 
 class a{
     public:
-    a(int x){
-        cout << "This is parent class  " << endl;
-        cout << "value of x is "<< x << endl;
+    virtual void print_d(){
+        cout << "This is class a" << endl;
     }
     
 };
 
 class b:public a{
     public:
-    b(int x,int y) : a(x){
-        cout << "This is child class 1 " << endl;
-        cout << "value of y " << y << endl;
+    void print_d(){
+        cout << "This is class b" << endl;
     }
+    
 };
 
 class c:public b{
     public:
-   c(int x, int y, int z) : b(x,y) {
-        cout << "This is child class 2 " << endl;
-        cout << "value of z is "<< z << endl;
+    void print_d(){
+        cout << "This is class c" << endl;
+        // b::print_d();
     }
+   
 };
 
 int main(){
-    c bb(10,20,30);
-    // bb.input();
-    // bb.a::input(); //this is to call function of parent class
+    a *ptr;
+    a aa;
+    ptr = &aa;
+    ptr -> print_d();
+
 }
