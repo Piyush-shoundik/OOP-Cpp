e4a9590cc5a19466c220a2491f74cbe8f084f849
Piyush-shoundik <ritetopiyush@gmail.com>
2026-03-16 01:25:16 +0530

    using operator overloading

--- polymorphism/main.cpp
+++ polymorphism/main.cpp
@@ -1,1 +1,32 @@
 //Getting started with polymorphism
+// operator overloading without constructor 
+
+#include <iostream>
+using namespace std;
+
+class add{
+    private:
+    int a;
+    public:
+    void getinput(){
+        cout << "Give input for a \n";
+        cin >> a;
+    }
+    void putinput(){
+        cout << a << " is the value for this object";
+    }
+    add operator+(add obj){
+        add sum;
+        sum.a = a + obj.a;
+        return sum;
+    }
+};
+
+int main(){
+    add aa,bb,cc;
+    aa.getinput();
+    bb.getinput();
+
+    cc = aa + bb ;
+    cc.putinput();
+}
